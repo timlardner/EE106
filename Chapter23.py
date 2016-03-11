f1757f16336e734e35b3141690a22dbebf309d86
Tim Lardner <timlarder@gmail.com>
2016-03-11 18:51:42 +0000

    Broken again. Ready for pull request

--- Chapter23.py
+++ Chapter23.py
@@ -3,7 +3,7 @@ def getIntegers():
     return int1,int2
 
 def doOr(int1, int2):
-    return (int1 or int2)# print the name to the console here
+    return # Do the 'or' operation and return either boolean true or false
 
 if __name__ == '__main__':
     int1, int2 = getIntegers()
