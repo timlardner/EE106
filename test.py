f02e79c1c10aef6be4a2b3a30cb999316867039e
Tim Lardner <timlarder@gmail.com>
2016-03-11 18:24:32 +0000

    Testing travis

---
+++ test.py
@@ -0,0 +1,14 @@
+import unittest
+from function import addition
+
+class TestFunctionMethods(unittest.TestCase):
+
+  def test_addition(self):
+      self.assertEqual(addition(1,1), 2)
+      self.assertEqual(addition(-1,1), 0)
+      self.assertEqual(addition(4,4), 8)
+      self.assertEqual(addition(-4,-4), -8)
+
+if __name__ == '__main__':
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestFunctionMethods)
+    unittest.TextTestRunner(verbosity=2).run(suite)
