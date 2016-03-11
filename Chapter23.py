8e9b9a78019282c60eda9ab0f1b9b83b0076dd10
Tim Lardner <timlarder@gmail.com>
2016-03-11 18:49:46 +0000

    Fixed or

--- Chapter23.py
+++ Chapter23.py
@@ -2,8 +2,8 @@ def getIntegers():
     int1, int2 = '', '' # Request the user's input here for two integers
     return int1,int2
 
-def doOr():
-    # print the name to the console here
+def doOr(int1, int2):
+    return (int1 or int2)# print the name to the console here
 
 if __name__ == '__main__':
     int1, int2 = getIntegers()
